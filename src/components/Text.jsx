6bc53875dad6131ac8f9ee92b6a56e9c4283d48b
CarlosMansoCid <carlosmanso12@gmail.com>
2022-08-13 00:14:03 -0400

    changes

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -3,12 +3,15 @@ import './Text.css';
 
 const Text = () =>{
     return(
-        <div className="text">
-            <h1 className="greats">Hi!</h1>
-            <br/>
-            <h2 className="name">I'm Carlos Alberto Manso Cid</h2>
-            <h3 className="position">frontend developer</h3>
+        <div className="text-container">
+            <div className="text">
+                <h1 className="greats">Hi!</h1>
+                <br/>
+                <h2 className="name">I'm Carlos Alberto Manso Cid</h2>
+                <h3 className="position">frontend developer</h3>
+            </div>
         </div>
+
 
     )
 }
